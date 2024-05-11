805e83469aae072ad4a6025ae9e713d43fd7a5da
Saranraj <www.saranrajcoder@gmail.com>
2024-05-11 22:50:00 +0530

    Product Card Fully Responsive

--- client/src/Components/Product/Product.jsx
+++ client/src/Components/Product/Product.jsx
@@ -4,8 +4,8 @@ import ProductCategory from './ProductList'
 function Product() {
     return (
         <div className='text-white'>
-            <h1 className='mt-[5rem] text-2xl font-bold text-center'>DO YOU WANT BUY NFT</h1>
-            <div className='grid grid-cols-10 gap-2 m-5 mt-[3rem]'>
+            <h1 className='sm:mt-[1rem] md:mt-[3rem] xl:mt-[5rem] text-2xl font-bold text-center'>DO YOU WANT BUY NFT</h1>
+            <div className='sm:mt-[1rem] sm:grid-cols-4 md:mt-[2rem] md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 xl:mt-[3rem] gap-2 m-5 grid'>
                 {ProductCategory.map((item,index) => (
                     <div key={index}>
                         <img className='mt-[0.5rem] rounded-lg' src={item.image} alt="" />
