f0111b954d844c97580ba626c8af1c4ddc12372c
Saranraj <www.saranrajcoder@gmail.com>
2024-05-11 15:24:23 +0530

    Navbar Fully Responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../Assets/png/token.png'
-
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar() {
-  return (
-    <div className='text-white'>
-        <img className='mt-3 ml-5 cursor-pointer' src={logo} alt="" />
-        <div>
-            <ul className='mt-[-1.5rem] mr-[3rem] font-bold flex justify-end gap-[3rem] '>
-                <li className='hover:text-gray-500 cursor-pointer'>Contact</li>
-                <li className='hover:text-gray-500 cursor-pointer'>Services</li>
-                <li className='hover:text-gray-500 cursor-pointer'>About</li>
-                <button className='bg-blue-500 cursor-pointer hover:bg-blue-900 h-[2rem] w-[5rem] rounded-md '>Login</button>
-            </ul>
+
+    const [nav, setNav] = useState(true);
+
+    const HandleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <div className='text-white'>
+            <img className='mt-3 ml-5 cursor-pointer' src={logo} alt="" />
+            <div>
+                <div onClick={HandleNav} className='mt-[-2rem] mr-[1rem] flex justify-end'>
+
+                    {!nav ? <IoMdClose className='sm:visible lg:invisible size-[2rem] ' /> : <IoMenu className='sm:visible lg:invisible size-[2rem] ' />}
+                </div>
+                <div className={!nav ? 'text-white fixed sm:left-0 sm:top-0 sm:w-[60%] sm:h-[15rem] sm:border-r border-r-gray-800 ease-in-out duration-200 sm:visible lg:invisible' : 'fixed left-[-100%]'}>
+                    <ul className='sm:visible sm:mt-[1rem] sm:ml-[1rem] sm:text-lg lg:invisible font-bold '>
+                        <li className='sm:mt-[1rem] hover:text-gray-500 cursor-pointer'>Contact</li>
+                        <li className='sm:mt-[1rem] hover:text-gray-500 cursor-pointer'>Services</li>
+                        <li className='sm:mt-[1rem] hover:text-gray-500 cursor-pointer'>About</li>
+                        <button className='sm:mt-[1rem] sm:ml-[-0.5rem] bg-blue-500 cursor-pointer hover:bg-blue-900 h-[2rem] w-[5rem] rounded-md '>Login</button>
+                    </ul>
+                </div>
+                <ul className='sm:invisible lg:visible mt-[-1.5rem] mr-[3rem] font-bold flex justify-end gap-[3rem] '>
+                    <li className='hover:text-gray-500 cursor-pointer'>Contact</li>
+                    <li className='hover:text-gray-500 cursor-pointer'>Services</li>
+                    <li className='hover:text-gray-500 cursor-pointer'>About</li>
+                    <button className='bg-blue-500 cursor-pointer hover:bg-blue-900 h-[2rem] w-[5rem] rounded-md '>Login</button>
+                </ul>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
