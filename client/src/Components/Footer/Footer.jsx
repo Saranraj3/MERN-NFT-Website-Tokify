453d0b67f9666556a39c9408eb3683e3156e05ed
Saranraj <www.saranrajcoder@gmail.com>
2024-05-12 21:02:38 +0530

    Footer Fully Responsive

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -8,13 +8,13 @@ import { FaCopyright } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className='overflow-x-hidden text-white bg-black h-screen  '>
-      <div className='mt-[3rem] ml-2 bg-black '>
+    <div className='overflow-x-hidden overflow-y-hidden text-white bg-black'>
+      <div className='sm:mt-[2rem] sm:text-center xl:mt-[3rem] xl:ml-2 bg-black '>
         <input className='ml-2 pl-2 cursor-default bg-black border h-[2.4rem] ' type="email" placeholder='E-mail' />
-        <button className='ml-2 text-lg font-bold hover:bg-blue-900 bg-blue-500 rounded-md h-[2.5rem] w-[5rem]'>Submit</button>
+        <button className='sm:mt-2 ml-2 text-lg font-bold hover:bg-blue-900 bg-blue-500 rounded-md h-[2.5rem] w-[5rem]'>Submit</button>
       </div>
       <div>
-        <div className='mt-[-2rem] flex justify-center bg-black'>
+        <div className='sm:mt-[2rem] xl:mt-[2rem] flex justify-center bg-black'>
           <h1 className='bg-black cursor-pointer'>JOIN </h1>
         </div>
         <div className=' flex justify-center gap-5 bg-black'>
@@ -30,19 +30,19 @@ function Footer() {
         <h1 className='ml-3 bg-black text-xl cursor-pointer font-bold'>Tokify</h1>
       </div>
       <div className='bg-black'>
-        <p className='mt-[1.5rem] ml-5 bg-black cursor-pointer max-w-[30rem]'>the use of non-fungible tokens (NFTs) to create a cooperative atmosphere for content creation, where NFT holders fund the project and contribute to the direction and narrative of the story</p>
+        <p className='sm:mt-[2rem] lg:mt-[1.5rem] ml-5 bg-black cursor-pointer max-w-[30rem]'>the use of non-fungible tokens (NFTs) to create a cooperative atmosphere for content creation, where NFT holders fund the project and contribute to the direction and narrative of the story</p>
       </div>
-      <div className='ml-[25rem] bg-black gap-[5rem] flex justify-center'>
-        <div className='mt-[-8rem] bg-black'>
+      <div className='sm:text-center sm:mt-[5rem] sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-5 lg:gap-[5rem] bg-black flex justify-center'>
+        <div className='lg:mt-[-0rem] bg-black'>
           <h1 className='cursor-pointer bg-black'>Market Place</h1>
-          <ul className='mt-[1rem] bg-black'>
+          <ul className='lg:mt-[1rem] bg-black'>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Art</li>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Gaming</li>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Membership</li>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Photography</li>
           </ul>
         </div>
-        <div className='mt-[-8rem] bg-black'>
+        <div className=' lg:mt-[-0rem] bg-black'>
           <h1 className='cursor-pointer bg-black'>My Account</h1>
           <ul className='mt-[1rem] bg-black'>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Profile</li>
@@ -52,7 +52,7 @@ function Footer() {
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Settings</li>
           </ul>
         </div>
-        <div className='mt-[-8rem] bg-black'>
+        <div className='sm:mt-[3rem] lg:mt-[-0rem] bg-black'>
           <h1 className='cursor-pointer bg-black'>Resources</h1>
           <ul className='mt-[1rem] bg-black'>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Blog</li>
@@ -65,7 +65,7 @@ function Footer() {
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>Parners</li>
           </ul>
         </div>
-        <div className='mt-[-8rem] bg-black'>
+        <div className='sm:mt-[3rem] lg:mt-[-0rem] bg-black'>
           <h1 className='cursor-pointer bg-black'>Company</h1>
           <ul className='mt-[1rem] bg-black'>
             <li className='mt-2 cursor-pointer hover:opacity-80 bg-black'>About</li>
